d6ea59d4802367e4a0886fb4594b56755d1ab38e
jaimejeg72 <jaimejeg72@gmail.com>
2026-07-29 09:41:07 -0400

    fix: update UXP install paths and manifest domains

--- install.bat
+++ install.bat
@@ -15,21 +15,26 @@ echo ========================================================
 echo.
 
 set "SOURCE_DIR=%~dp0"
-set "TARGET_DIR=C:\Program Files\Common Files\Adobe\UXP\Plugins\External\FrostflowPlugin"
+set "TARGET_DIR1=C:\Program Files\Common Files\Adobe\UXP\Plugins\External\FrostflowPlugin"
+set "TARGET_DIR2=C:\Program Files\Common Files\Adobe\UXP\Plugins\External\PS\FrostflowPlugin"
 
-if not exist "%TARGET_DIR%" (
-    echo Creando directorio de destino:
-    echo "%TARGET_DIR%"
-    mkdir "%TARGET_DIR%"
-)
+if not exist "%TARGET_DIR1%" mkdir "%TARGET_DIR1%"
+if not exist "%TARGET_DIR2%" mkdir "%TARGET_DIR2%"
 
-echo Copiando archivos del plugin...
-copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR%\" >nul
-copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR%\" >nul
-copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR%\" >nul
-copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR%\" >nul
-if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR%\" >nul
-if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR%\" >nul
+echo Copiando archivos del plugin a directorios UXP...
+copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR1%\" >nul
+copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR1%\" >nul
+copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR1%\" >nul
+copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR1%\" >nul
+if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR1%\" >nul
+if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR1%\" >nul
+
+copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR2%\" >nul
+copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR2%\" >nul
+copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR2%\" >nul
+copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR2%\" >nul
+if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR2%\" >nul
+if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR2%\" >nul
 
 echo.
 echo ========================================================
@@ -37,7 +42,8 @@ echo   ¡Instalacion completada con exito!
 echo.
 echo   Pasos siguientes:
 echo   1. Abre o reinicia Adobe Photoshop.
-echo   2. Ve al menu superior: Plugins -> FrostFlow Tareas.
+echo   2. Ve al menu superior: Plugins -> Plugins Panel
+echo      (o Plugins -> FrostFlow Tareas)
 echo ========================================================
 echo.
 pause
