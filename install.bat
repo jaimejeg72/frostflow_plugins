b793f1098f98dd72e181d044cb394f90641dbcde
jaimejeg72 <jaimejeg72@gmail.com>
2026-07-29 09:50:06 -0400

    fix: copy to all user and system UXP plugin paths

--- install.bat
+++ install.bat
@@ -17,11 +17,15 @@ echo.
 set "SOURCE_DIR=%~dp0"
 set "TARGET_DIR1=C:\Program Files\Common Files\Adobe\UXP\Plugins\External\FrostflowPlugin"
 set "TARGET_DIR2=C:\Program Files\Common Files\Adobe\UXP\Plugins\External\PS\FrostflowPlugin"
+set "TARGET_DIR3=%APPDATA%\Adobe\UXP\Plugins\External\FrostflowPlugin"
+set "TARGET_DIR4=%APPDATA%\Adobe\UXP\Plugins\External\PS\FrostflowPlugin"
 
 if not exist "%TARGET_DIR1%" mkdir "%TARGET_DIR1%"
 if not exist "%TARGET_DIR2%" mkdir "%TARGET_DIR2%"
+if not exist "%TARGET_DIR3%" mkdir "%TARGET_DIR3%"
+if not exist "%TARGET_DIR4%" mkdir "%TARGET_DIR4%"
 
-echo Copiando archivos del plugin a directorios UXP...
+echo Copiando archivos del plugin a todos los directorios UXP de Photoshop...
 copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR1%\" >nul
 copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR1%\" >nul
 copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR1%\" >nul
@@ -36,14 +40,27 @@ copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR2%\" >nul
 if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR2%\" >nul
 if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR2%\" >nul
 
+copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR3%\" >nul
+copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR3%\" >nul
+copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR3%\" >nul
+copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR3%\" >nul
+if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR3%\" >nul
+if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR3%\" >nul
+
+copy /Y "%SOURCE_DIR%manifest.json" "%TARGET_DIR4%\" >nul
+copy /Y "%SOURCE_DIR%index.html" "%TARGET_DIR4%\" >nul
+copy /Y "%SOURCE_DIR%main.js" "%TARGET_DIR4%\" >nul
+copy /Y "%SOURCE_DIR%styles.css" "%TARGET_DIR4%\" >nul
+if exist "%SOURCE_DIR%logo_white.png" copy /Y "%SOURCE_DIR%logo_white.png" "%TARGET_DIR4%\" >nul
+if exist "%SOURCE_DIR%README.md" copy /Y "%SOURCE_DIR%README.md" "%TARGET_DIR4%\" >nul
+
 echo.
 echo ========================================================
-echo   ¡Instalacion completada con exito!
+echo   ¡Instalacion completada con exito en todos los entornos!
 echo.
 echo   Pasos siguientes:
-echo   1. Abre o reinicia Adobe Photoshop.
-echo   2. Ve al menu superior: Plugins -> Plugins Panel
-echo      (o Plugins -> FrostFlow Tareas)
+echo   1. Reinicia Adobe Photoshop.
+echo   2. Ve al menu superior: Plugins -> FrostFlow Tareas
 echo ========================================================
 echo.
 pause
